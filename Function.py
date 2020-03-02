3fa8062e1a863604c9851540c9a29aff559a7f24
Xin Jin <xjin.comm@gmail.com>
2020-03-02 15:54:16 +0800

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,49 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def generate_path(path):\n",
+    "    folder = os.path.exists(path)\n",
+    "    if not folder:\n",
+    "        os.makedirs(path)  "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def video(video_url,file_name):\n",
+    "    return urllib.request.urlretrieve(video_url,file_name)\n",
+    "def cover(cover_url,file_name):\n",
+    "    return urllib.request.urlretrieve(cover_url,file_name)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
